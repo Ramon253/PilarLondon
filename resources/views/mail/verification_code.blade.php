07cd5b9dd25e80038bc7664471abeaaf16bf0406
Ramon253 <114581896+Ramon253@users.noreply.github.com>
2024-06-18 09:36:35 +0200

    Update verification_code.blade.php

--- resources/views/mail/verification_code.blade.php
+++ resources/views/mail/verification_code.blade.php
@@ -5,7 +5,7 @@
 <body>
 <div style="padding: 1rem; background: #464450; color: whitesmoke; border-radius: 0.5rem; text-align: center">
     <h1 style="color: white">Bienvenido a Pilar london - Academia</h1>
-    <p style="color: lightgrey">Gracias por registrarte en VOST. Tu codigo de activacion es:</p>
+    <p style="color: lightgrey">Gracias por registrarte en Pilarlondon. Tu codigo de activacion es:</p>
     <p style="font-size: 24px; font-weight: bold;"> {{$code}}</p>
     <p style="color: lightgrey">Ingresa este codigo en la plataforma para activar tu cuenta.</p>
 </div>
